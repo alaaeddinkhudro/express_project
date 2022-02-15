ce7b019b0d8b0a71adfaa25307d67cb86bda37ce
alaaeddin <alaaeddin.khudro@miniandmore.co>
2022-02-15 15:11:43 +0400

    create post endpoint

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const body_parser = require('body-parser');
+
 const app = express();
+app.use(body_parser.json()); // parse json in body
+app.use(body_parser.urlencoded({
+    extended: true
+})); // parse fields in x-www-form-urlencoded
+
 
 let customers = [{
         id: 1,
@@ -27,5 +34,16 @@ app.get('/customers/:id', (req, res) => {
 
     res.send(JSON.stringify(customer));
 });
+
+app.post('/customers', (req, res) => {
+    const customer = {
+        id: customers.length + 1,
+        name: req.body.name
+    };
+
+    customers.push(customer);
+    res.status(201).send(customer);
+});
+
 
 app.listen(8080);
