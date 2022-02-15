023c901779af539e0d99e739fc5e41b5c71c9f48
alaaeddin <alaaeddin.khudro@miniandmore.co>
2022-02-15 12:28:08 +0400

    adding get end point

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+let customer = [{
+        id: 1,
+        name: 'alaa'
+    },
+    {
+        id: 2,
+        name: 'amin'
+    },
+    {
+        id: 3,
+        name: 'muaaz'
+    },
+]
+
+app.get('/customers', (req, res) => {
+    res.send(JSON.stringify(customer));
 });
 
 app.listen(8080);
